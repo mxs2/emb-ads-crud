47009ecbc85729af0868c7e7169923cbcbfc90bb
Raphael Miranda <rapheto@hotmail.com>
2024-10-28 18:24:15 -0300

    add menu_inicial and update main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,20 @@
+import json
+import os
+
+def menu_inicial():
+    print("ADOTE PET")
+    print("1 - MÓDULO DO USUÁRIOS ")
+    print("2 - ADOTE UM PET")
+    print("3 - MÓDULO DO ANIMAL")
+    print("4 - SAIR ")
+
 def main(): 
-    print("Oi")
+    while True:
+        menu_inicial()
+        opcao_inicial = int(input("Escolha uma opção:\n>>>"))
+        match opcao_inicial:
+            case(1):
+                print("Op 1")
 
 if __name__ == "__main__":
     main()
