407d8cbac964f5fb40030c52e2b40b1503b16e88
Raphael Miranda <rapheto@hotmail.com>
2024-11-02 18:53:52 -0300

    add adocaoData
    linkei o json no arquivo de adocao

--- src/adocao.py
+++ src/adocao.py
@@ -1,3 +1,10 @@
 import json
 import os
 
+adocaoData = os.path.join(os.path.dirname(__file__), 'adocaoData.json')
+if not os.path.exists(adocaoData):
+    with open(adocaoData, 'w') as adocaoDataArquivo:
+        json.dump({}, adocaoDataArquivo)
+#Escrever dados no json
+"""with open(adocaoData, 'w') as adocaoDataArquivo:
+        json.dump(dados, adocaoDataArquivo, indent=4)"""
