da1a40360770524867f3c052828265f7ac3e50b0
Raphael Miranda <rapheto@hotmail.com>
2024-11-09 11:59:13 -0300

    Refactor perfilAdocao and test data db
    tirei algumas linhas sem uso e mudei a forma que a listagem de perfilAdocao é listada no terminal

--- src/perfilAdocao.py
+++ src/perfilAdocao.py
@@ -2,7 +2,6 @@ import json
 import os
 
 def Adicionar_animal():
-    
     print("Vamos cadastrar seu Pet! Ele pode fazer outra família muito feliz, nos ajude a encontrar o macth perfeito para seu bichinho!")
     nome = input("Como devemos chamar seu pet? ")
     especie = input("Qual é a espécie do seu pet? (Exemplo: Cachorro, Gato, etc.) ")
@@ -27,8 +26,7 @@ def Adicionar_animal():
         "Corretamente Vacinado? ": vacinas.upper(),
         "Alguma informacao relevante? ": info_relevante
     }
-    
-    
+       
     if os.path.exists("data.json") and os.path.getsize("data.json") > 0:
         with open("data.json", "r") as getdata:
             data = json.load(getdata)
@@ -42,41 +40,32 @@ def Adicionar_animal():
     
     print(f"Animal Cadastrado! O token do seu processo é: {len(data) - 1}")
 
-
-
 def ListarAnimais(): 
     with open("data.json", "r") as view:
         data = json.load(view)
-        
     for i, m in data.items():
 
         caracteristicas = [f"Animal número: {i}"]
         for x, n in m.items():
             caracteristicas.append(f"{x} {n}")
         
-        print("  -  ".join(caracteristicas))
+        print("\n".join(caracteristicas))
         print("\r") 
         
 """def DeletarAdocao():
     with open("data.json", "r") as view:
         data = json.load(view)
     print("")"""
-    
-
 def _main_():
     check = True
     while check:
         print("Opções: \n --> 1 CRIAR \n --> 2 VIZUALIZAR")
-        escolha= input("Digite sua escolha! --> ")
-        
+        escolha= input("Digite sua escolha! --> ")      
         match escolha:
             case "1":
                 Adicionar_animal()
             case "2":
                 ListarAnimais()
             case __:
-                check = False
-                
+                check = False             
 _main_()
-
-    
